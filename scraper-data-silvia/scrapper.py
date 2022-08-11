2c449f71df944180a00116fc94d38060d43ed253
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-08-11 00:41:53 -0500

    update scrapper and scrapper2
    - update in scrapper and deleting scrapper2 with function

--- scraper-data-silvia/scrapper.py
+++ scraper-data-silvia/scrapper.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from bs4 import BeautifulSoup
+import re
 
 topics = ['technology',
           'productivity',
@@ -35,34 +36,36 @@ for word in topics:
     soup = BeautifulSoup(driver.page_source, features="lxml")
 
     html = soup.find_all(id="video-title")
-
-    while len(html) < 230:
+    print("----------------------------- Scrolling Youtube for videos -----------------------------")
+    while len(html) < 220:
           driver.execute_script("let scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")
           soup = BeautifulSoup(driver.page_source, features="lxml")
           html =  soup.find_all("a", id="video-title", class_="yt-simple-endpoint")
-
+    print("----------------------------- Starting scraping of topic -----------------------------", word)
     for l in html:
         if l != None:
             link = l.get('href')
             response = requests.get(f'https://www.youtube.com{link}')
 
             soup = BeautifulSoup(response.text, features="lxml")
+            patron_description = re.compile('shortDescription":"(.*)","isCrawlable')
 
             keep_videos.append(
             {
-            'url': f'https://www.youtube.com{link}',
-            'title': (soup.find("meta", {"name": "title"})['content']).strip().replace("\n", " "),
-            'description': (soup.find("meta", {"name": "description"})['content']).strip().replace("\n", " "),
-            'tags': soup.find("meta", {"name": "keywords"})['content'],
-            'thumbnail': soup.find("link", {"rel": "image_src"})['href']
+                'url': f'https://www.youtube.com{link}',
+                'title': (soup.find("meta", {"name": "title"})['content']).strip().replace("\n", " "),
+                'description': (patron_description.findall(str(soup))[0]).strip().replace("\n", " "),
+                'tags': soup.find("meta", {"name": "keywords"})['content'],
+                'thumbnail': soup.find("link", {"rel": "image_src"})['href']
             })
-
+            print(link, "---had been scrapped successfully---")
+    print("----------------------------- Finishing scraping of topic -----------------------------", word)
     fin = time.time()
 
-    print((fin - inicio)/60)
-    print(fin - inicio)
+    print((fin - inicio)/60, "minutos parciales")
+    print(fin - inicio, "segundos parciales")
 
-print((fin - inicio)/60)
-print(fin - inicio)
+print((fin - inicio)/60, "minutos totales")
+print(fin - inicio, "segundos totales")
 with open('./scraper-data-silvia/data.json', 'w+', encoding='utf8') as json_file:
           json.dump({'videos': keep_videos}, json_file, ensure_ascii=False)
