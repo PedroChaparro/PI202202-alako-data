e87c5d85760ed8ea2f68639576f5947c7fc0fe2c
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-08-13 11:22:34 -0500

    Update scrapper.py
    ´lil change to prove it in linux and windows

--- scraper-data-silvia/scrapper.py
+++ scraper-data-silvia/scrapper.py
@@ -1,5 +1,6 @@
 import json
 import time
+from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,16 +10,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-topics = ['technology',
-          'productivity',
-          'a day in the life of',
-          'recipes',
-          'cleaning recommendations',
-          'news',
-          'sport news',
-          'student vlogs',
-          'study advices',
-          'tasting food']
+topics = ['technology']
 
 keep_videos = []
 
@@ -37,14 +29,15 @@ for word in topics:
 
     html = soup.find_all(id="video-title")
     print("----------------------------- Scrolling Youtube for videos -----------------------------")
-    while len(html) < 220:
+    while len(html) < 10:
           driver.execute_script("let scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")
           soup = BeautifulSoup(driver.page_source, features="lxml")
           html =  soup.find_all("a", id="video-title", class_="yt-simple-endpoint")
     print("----------------------------- Starting scraping of topic -----------------------------", word)
     for l in html:
-        if l != None:
-            link = l.get('href')
+        link = l.get('href')
+        if link != None:
+            print(link)
             response = requests.get(f'https://www.youtube.com{link}')
 
             soup = BeautifulSoup(response.text, features="lxml")
@@ -53,8 +46,8 @@ for word in topics:
             keep_videos.append(
             {
                 'url': f'https://www.youtube.com{link}',
-                'title': (soup.find("meta", {"name": "title"})['content']).strip().replace("\n", " "),
-                'description': (patron_description.findall(str(soup))[0]).strip().replace("\n", " "),
+                'title': (soup.find("meta", {"name": "title"})['content']).strip().replace('\\n', ' ').replace('\\', '').replace('  ', ' '),
+                'description': (patron_description.findall(str(soup))[0]).strip().replace('\\n', ' ').replace('\\', '').replace('  ', ' '),
                 'tags': soup.find("meta", {"name": "keywords"})['content'],
                 'thumbnail': soup.find("link", {"rel": "image_src"})['href']
             })
@@ -67,5 +60,5 @@ for word in topics:
 
 print((fin - inicio)/60, "minutos totales")
 print(fin - inicio, "segundos totales")
-with open('./scraper-data-silvia/data.json', 'w+', encoding='utf8') as json_file:
+with open(Path('./scraper-data-silvia/data.json'), 'w+', encoding='utf-8') as json_file:
           json.dump({'videos': keep_videos}, json_file, ensure_ascii=False)
